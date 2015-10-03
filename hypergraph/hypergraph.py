316ddc454e479513bd53e333b6ac0f1772780996
Leland McInnes <leland.mcinnes@gmail.com>
2015-10-03 14:57:42 -0400

    Documentation

--- hypergraph/hypergraph.py
+++ hypergraph/hypergraph.py
@@ -19,16 +19,47 @@ class Hypergraph (object):
 			for node in nodes:
 				self.nodes[node] = POMSet([])
 
-        def nodes(self):
-                return self.node.keys()
+    def nodes(self):
+    	'''Return a list (or iterable in python3) of the node
+    	objects of the hypergraph.
+    	'''
+        return self.node.keys()
 
-        def edges(self):
-                return self.edge.keys()
+    def edges(self):
+    	'''Return a list (or iterable in python3) of the edge
+    	objects of the hypergraph.
+    	'''
+         return self.edge.keys()
 
 	def add_node(self, new_node):
+		'''Add a new node to the hypergraph.
+
+		Parameters
+		----------
+
+		new_node : object
+			The new node to add to the hypergraph
+		'''
 		self.nodes[new_node] = POMSet([])
 
 	def add_edge(self, new_edge, edge_labels, edge_order=None):
+		'''Add a new edge to the hypergraph.
+
+		Parameters
+		----------
+		new_edge : object
+			The edge object to add to the hypergraph.
+
+		edge_labels : iterable
+			An iterable of node objects that the edge relates.
+			If the objects are not already nodes of the hypergraph
+			then new nodes will be added to the hypergraph for them.
+
+		edge_order : numpy ndarray (len(edge_labels), len(edge_labels)), optional
+			The POMSET order of the edge, or None. If None the
+			edge will be undirected (but can have dependencies added).
+			(default None)
+		'''
 		self.edges[new_edge] = POMSet(edge_labels, edge_order)
 
 		for node in edge_labels:
@@ -37,12 +68,18 @@ class Hypergraph (object):
 			self.nodes[node].add_label(new_edge)
 
 	def dual(self):
+		'''Return a new hypergraph that is the dual of the current
+		hypergraph.
+		'''
 		result = Hypergraph()
 		result.nodes = self.edges.copy()
 		result.edges = self.nodes.copy()
 		return result
 
 	def networkx_bipartite_representation(self):
+		'''Return a NetworkX graph of the bipartite representation of the 
+		hypergraph.
+		'''
 		result = nx.Graph()
 		result.add_nodes_from(self.nodes.keys() + self.edges.keys())
 		for edge in self.edges:
@@ -51,7 +88,10 @@ class Hypergraph (object):
 
 		return result
 
-	def networkx_undirected_two_section(self):
+	def networkx_undirected_cliquification(self):
+		'''Return a NetworkX graph derived from the hypergraph by
+		converting all hyperegdes into graph cliques.
+		'''
 		result = nx.Graph()
 		result.add_nodes_from(self.nodes.keys())
 		for edge in self.edges:
