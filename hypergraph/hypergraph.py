0772d0c28ab1ade85917ab0964192c31b3f55dd2
Leland McInnes <leland.mcinnes@gmail.com>
2015-10-03 15:16:11 -0400

    Add extra cliquifications and clean up

--- hypergraph/hypergraph.py
+++ hypergraph/hypergraph.py
@@ -40,7 +40,7 @@ class Hypergraph (object):
 		new_node : object
 			The new node to add to the hypergraph
 		'''
-		self.nodes[new_node] = POMSet([])
+		self.node[new_node] = POMSet([])
 
 	def add_edge(self, new_edge, edge_labels, edge_order=None):
 		'''Add a new edge to the hypergraph.
@@ -60,30 +60,42 @@ class Hypergraph (object):
 			edge will be undirected (but can have dependencies added).
 			(default None)
 		'''
-		self.edges[new_edge] = POMSet(edge_labels, edge_order)
+		self.edge[new_edge] = POMSet(edge_labels, edge_order)
 
 		for node in edge_labels:
 			if node not in self.nodes:
 				self.add_node(node)
-			self.nodes[node].add_label(new_edge)
+			self.node[node].add_label(new_edge)
 
 	def dual(self):
 		'''Return a new hypergraph that is the dual of the current
 		hypergraph.
+
+		Returns
+		-------
+
+		dual : Hypergraph
+			The dual of the hypergraph.
 		'''
 		result = Hypergraph()
-		result.nodes = self.edges.copy()
-		result.edges = self.nodes.copy()
+		result.node = self.edge.copy()
+		result.edge = self.node.copy()
 		return result
 
 	def networkx_bipartite_representation(self):
 		'''Return a NetworkX graph of the bipartite representation of the 
 		hypergraph.
+
+		Returns
+		-------
+
+		graph : NetworkX Graph
+			The bipartite representation graph.
 		'''
 		result = nx.Graph()
-		result.add_nodes_from(self.nodes.keys() + self.edges.keys())
+		result.add_nodes_from(self.nodes() + self.edges())
 		for edge in self.edges:
-			for label in self.edges[edge].labels:
+			for label in self.edge[edge].labels:
 				result.add_edge(edge, label)
 
 		return result
@@ -91,11 +103,67 @@ class Hypergraph (object):
 	def networkx_undirected_cliquification(self):
 		'''Return a NetworkX graph derived from the hypergraph by
 		converting all hyperegdes into graph cliques.
+
+		Returns
+		-------
+
+		graph : NetworkX Graph
+			The cliquified graph.
 		'''
 		result = nx.Graph()
-		result.add_nodes_from(self.nodes.keys())
-		for edge in self.edges:
-			for n1, n2 in itr.combinations(self.edges[edge].labels, 2):
+		result.add_nodes_from(self.nodes())
+		for edge in self.edge:
+			for n1, n2 in itr.combinations(self.edge[edge].labels, 2):
 				result.add_edge(n1, n2)
+
+		return result
+
+	def networkx_weakly_directed_cliquification(self):
+		'''Return a NetworkX graph derived from the hypergraph by 
+		converting hypergraph edges into graph cliques weakly respecting
+		directedness  of hyperedges.
+
+		That is, we create a graph edge between node `i` and node `j` if
+		there exists a hyperedge that includes nodes `i` and `j` such
+		that `j` is greater than or unrelated to `i` in that edge.
+
+		Returns
+		-------
+
+		graph : NetworkX Graph
+			The cliquified graph.
+		'''
+		result = nx.Graph()
+		result.add_nodes_from(self.nodes())
+		for edge in self.edge:
+			for n1 in self.edge[edge].support:
+				for n1_index in range(self.edge[edge].multiplicity(n1)):
+					for n2 in self.edge[edge].weakly_above(n1, n1_index):
+						result.add_edge(n1, n2)
+
+		return result
+
+	def networkx_strictly_directed_cliquification(self):
+		'''Return a NetworkX graph derived from the hypergraph by 
+		converting hypergraph edges into graph cliques strictly respecting
+		directedness  of hyperedges.
+
+		That is, we create a graph edge between node `i` and node `j` if
+		there exists a hyperedge that includes nodes `i` and `j` such
+		that `j` is strictly greater than `i` in that edge.
+
+		Returns
+		-------
+
+		graph : NetworkX Graph
+			The cliquified graph.
+		'''
+		result = nx.Graph()
+		result.add_nodes_from(self.nodes())
+		for edge in self.edge:
+			for n1 in self.edge[edge].support:
+				for n1_index in range(self.edge[edge].multiplicity(n1)):
+					for n2 in self.edge[edge].strictly_above(n1, n1_index):
+						result.add_edge(n1, n2)
 
 		return result
