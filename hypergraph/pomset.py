23fc2df639a38acd68ee9fdd80d52cf94fbc5319
Leland McInnes <leland.mcinnes@gmail.com>
2015-10-05 15:35:08 -0400

    Continue bipartition; cleanup according to PyCharm

--- hypergraph/pomset.py
+++ hypergraph/pomset.py
@@ -39,12 +39,12 @@ def _make_order_from_bipartition(bipartition):
     return result
 
 class POMSet (object):
-    '''A Partially Ordered Multiset.
+    """A Partially Ordered Multiset.
 
     A POMSet is composed of labels (the items in the multiset) and
     an order (the partial order of the labels). Partial orders are
     provided as 2 dimensional numpy ndarray specifying order relations
-    on the `i`th and `j`th labels as 
+    on the `i`th and `j`th labels as
         order[i,j] == 1  <==> labels[i] > label[j]
         order[i,j] == 0  <==> label[i] is unrelated to label[j]
         order[i,j] == -1 <==> label[i] < label[j]
@@ -52,7 +52,7 @@ class POMSet (object):
     The `size` of a POMSet is the number of labels.
     The `support` of a POMSet is the set of distinct labels.
     The `cardinality` of a POMSet is the size of the support.
-    '''
+    """
 
     def __init__(self, labels=None, order=None, bipartition=None):
 
@@ -66,13 +66,13 @@ class POMSet (object):
         self.cardinality = len(self.support)
 
         self._is_unordered = False
-        self._is_bipartitite = False
+        self._is_bipartite = False
         self._bipartition = None
 
         if bipartition is not None:
             assert(order is None)
             assert(sum(len(x) for x in bipartition) == len(self.labels))
-            self._is_bipartitite = True
+            self._is_bipartite = True
             self._bipartition = bipartition
             self.order = _make_order_from_bipartition(bipartition)
 
@@ -92,7 +92,7 @@ class POMSet (object):
             self._is_unordered = True
 
     def multiplicity(self, element):
-        '''Return the number of occurences of `element` in the POMSet.
+        """Return the number of occurences of `element` in the POMSet.
 
         Parameters
         ----------
@@ -105,22 +105,22 @@ class POMSet (object):
 
         multiplicity : int
             The multiplicity of `element` in this POMSet.
-        '''
+        """
         return np.sum(self.labels == element)
 
     def reverse_order(self):
-    	'''Perform an in place order reversal on the POMSet.
+        """Perform an in place order reversal on the POMSet.
 
-    	Thus if previously i > j, this method will result in
-    	i < j in the POMSet order.
-    	'''
+        Thus if previously i > j, this method will result in
+        i < j in the POMSet order.
+        """
         if not self._is_unordered:
             self.order = self.order.T
 
     def weakly_above(self, element, element_index=0):
-        '''Get all elements of the POMSet that are weakly above `element`.
+        """Get all elements of the POMSet that are weakly above `element`.
 
-        Here weakly above means elements that are either strictly greater than 
+        Here weakly above means elements that are either strictly greater than
         or unrelated to `element` in the POSET order.
 
         Parameters
@@ -140,22 +140,38 @@ class POMSet (object):
 
         labels_above : numpy ndarray
             A numpy array of label objects weakly above `element`
-        '''
+        """
         if self._is_unordered:
             return self.labels.copy()
+
         label_index = np.where(self.labels == element)[0][element_index]
+
+        if self._is_bipartite:
+            if label_index in self._bipartition[0]:
+                return self.labels.copy()
+            else:
+                return self.labels[self._bipartition[1]]
+
         return self.labels[~(self.order[label_index] == -1)]
 
     def _indices_strictly_above(self, element, element_index=0):
         if self._is_unordered:
             return np.array([], dtype=int)
+
         label_index = np.where(self.labels == element)[0][element_index]
-        return np.where[(self.order[label_index] == 1)][0]
+
+        if self._is_bipartite:
+            if label_index in self._bipartition[0]:
+                return np.array(self._bipartition[1])
+            else:
+                return np.array([], dtype=int)
+
+        return np.where(self.order[label_index] == 1)[0]
 
     def strictly_above(self, element, element_index=0):
-        '''Get all elements of the POMSet that are strictly above `element`.
+        """Get all elements of the POMSet that are strictly above `element`.
 
-        Here weakly above means elements that are strictly greater than 
+        Here weakly above means elements that are strictly greater than
         `element` in the POSET order.
 
         Parameters
@@ -175,16 +191,24 @@ class POMSet (object):
 
         labels_above : numpy ndarray
             A numpy array of label objects strictly above `element`
-        '''
+        """
         if self._is_unordered:
             return np.array([], dtype=object)
+
         label_index = np.where(self.labels == element)[0][element_index]
+
+        if self._is_bipartite:
+            if label_index in self._bipartition[0]:
+                return self.labels[self._bipartition[1]]
+            else:
+                return np.array([], dtype=object)
+
         return self.labels[(self.order[label_index] == 1)]
 
     def weakly_below(self, element, element_index=0):
-        '''Get all elements of the POMSet that are weakly below `element`.
+        """Get all elements of the POMSet that are weakly below `element`.
 
-        Here weakly above means elements that are either strictly less than 
+        Here weakly above means elements that are either strictly less than
         or unrelated to `element` in the POSET order.
 
         Parameters
@@ -204,22 +228,38 @@ class POMSet (object):
 
         labels_below : numpy ndarray
             A numpy array of label objects weakly below `element`
-        '''
+        """
         if self._is_unordered:
             return self.labels.copy()
+
         label_index = np.where(self.labels == element)[0][element_index]
+
+        if self._is_bipartite:
+            if label_index in self._bipartition[1]:
+                return self.labels.copy()
+            else:
+                return self.labels[self._bipartition[0]]
+
         return self.labels[~(self.order[label_index] == 1)]
 
     def _indices_strictly_below(self, element, element_index=0):
         if self._is_unordered:
             return np.array([], dtype=int)
+
         label_index = np.where(self.labels == element)[0][element_index]
-        return np.where[(self.order[label_index] == -1)]
+
+        if self._is_bipartite:
+            if label_index in self._bipartition[1]:
+                return self.labels[self._bipartition[0]]
+            else:
+                return np.array([], dtype=int)
+
+        return np.where(self.order[label_index] == -1)[0]
 
     def strictly_below(self, element, element_index=0):
-        '''Get all elements of the POMSet that are strictly below `element`.
+        """Get all elements of the POMSet that are strictly below `element`.
 
-        Here weakly above means elements that are strictly less than 
+        Here weakly above means elements that are strictly less than
         `element` in the POSET order.
 
         Parameters
@@ -239,14 +279,22 @@ class POMSet (object):
 
         labels_below : numpy ndarray
             A numpy array of label objects strictly below `element`
-        '''
+        """
         if self._is_unordered:
             return np.array([], dtype=object)
+
         label_index = np.where(self.labels == element)[0][element_index]
+
+        if self._is_bipartite:
+            if label_index in self._bipartition[1]:
+                return self.labels[self._bipartition[0]]
+            else:
+                return np.array([], dtype=object)
+
         return self.labels[(self.order[label_index] == -1)]
 
     def weakly_greater_than(self, element1, element2, element1_index=0, element2_index=0):
-        '''Report whether `element1` is weakly greater than `element2`.
+        """Report whether `element1` is weakly greater than `element2`.
 
         In this case weakly greater than means not strictly less than.
 
@@ -276,15 +324,15 @@ class POMSet (object):
 
         weakly_greater_than : boolean
             Whether `element1` is weakly greater than `element2`
-        '''
+        """
         if self._is_unordered:
             return True
-        label_index1 = np.where(self.labels == element1)[0][element_index1]
-        label_index2 = np.where(self.labels == element2)[0][element_index2]
+        label_index1 = np.where(self.labels == element1)[0][element1_index]
+        label_index2 = np.where(self.labels == element2)[0][element2_index]
         return self.order[label_index1, label_index2] >= 0
 
     def strictly_greater_than(self, element1, element2, element1_index=0, element2_index=0):
-        '''Report whether `element1` is strictly greater than `element2`.
+        """Report whether `element1` is strictly greater than `element2`.
 
         Parameters
         ----------
@@ -312,15 +360,15 @@ class POMSet (object):
 
         strictly_greater_than : boolean
             Whether `element1` is strictly greater than `element2`
-        '''
+        """
         if self._is_unordered:
             return False
-        label_index1 = np.where(self.labels == element1)[0][element_index1]
-        label_index2 = np.where(self.labels == element2)[0][element_index2]
+        label_index1 = np.where(self.labels == element1)[0][element1_index]
+        label_index2 = np.where(self.labels == element2)[0][element2_index]
         return self.order[label_index1, label_index2] > 0
 
     def weakly_less_than(self, element1, element2, element1_index=0, element2_index=0):
-        '''Report whether `element1` is weakly less than `element2`.
+        """Report whether `element1` is weakly less than `element2`.
 
         In this case weakly less than means not strictly greater than.
 
@@ -350,15 +398,15 @@ class POMSet (object):
 
         weakly_less_than : boolean
             Whether `element1` is weakly less than `element2`
-        '''
+        """
         if self._is_unordered:
             return True
-        label_index1 = np.where(self.labels == element1)[0][element_index1]
-        label_index2 = np.where(self.labels == element2)[0][element_index2]
+        label_index1 = np.where(self.labels == element1)[0][element1_index]
+        label_index2 = np.where(self.labels == element2)[0][element2_index]
         return self.order[label_index1, label_index2] <= 0
 
     def strictly_less_than(self, element1, element2, element1_index=0, element2_index=0):
-        '''Report whether `element1` is strictly less than `element2`.
+        """Report whether `element1` is strictly less than `element2`.
 
         Parameters
         ----------
@@ -386,17 +434,17 @@ class POMSet (object):
 
         strictly_less_than : boolean
             Whether `element1` is strictly less than `element2`
-        '''
+        """
         if self._is_unordered:
             return False
-        label_index1 = np.where(self.labels == element1)[0][element_index1]
-        label_index2 = np.where(self.labels == element2)[0][element_index2]
+        label_index1 = np.where(self.labels == element1)[0][element1_index]
+        label_index2 = np.where(self.labels == element2)[0][element2_index]
         return self.order[label_index1, label_index2] < 0
 
     def add_label(self, new_label):
-        '''Add a new element to the POMSet. The added element will be
+        """Add a new element to the POMSet. The added element will be
         unrelated to any other elements in the POMSet; to induce relations
-        after using this method use the `add_dependency` or 
+        after using this method use the `add_dependency` or
         `add_dependencies_from` functions.
 
         Parameters
@@ -404,7 +452,7 @@ class POMSet (object):
 
         new_label : object
             The new element to add to the POMSet.
-        '''
+        """
         new_label_array = np.empty(self.size + 1, dtype=object)
         new_label_array[:-1] = self.labels
         del self.labels
@@ -421,7 +469,7 @@ class POMSet (object):
         self.order = new_order
 
     def add_dependency(self, from_label, to_label, from_index=0, to_index=0):
-        '''Add a new dependency relation to the POMSet. This states that
+        """Add a new dependency relation to the POMSet. This states that
         `from_label` is strictly less than `to_label`. All other relations
         implicit from this will then be inferred and also added.
 
@@ -446,7 +494,7 @@ class POMSet (object):
             e.g. `to_index=3` will select the third copy of to_element
             within the label list. (default 0)
 
-        '''
+        """
         from_label_index = np.where(self.labels == from_label)[0][from_index]
         to_label_index = np.where(self.labels == to_label)[0][to_index]
 
@@ -458,10 +506,10 @@ class POMSet (object):
         self._is_unordered = False
 
     def add_labels_from(self, new_label_list):
-        '''
+        """
         Add a number of new labels from an iterable of new labels.
         The added elements will be unrelated to any other elements
-        in the POMSet. To add relations use the `add_dependency` or 
+        in the POMSet. To add relations use the `add_dependency` or
         `add_dependencies_from` functions.
 
         Parameters
@@ -469,12 +517,12 @@ class POMSet (object):
 
         new_label_list : iterable
             The new labels to be added
-        '''
+        """
         labels_to_add = np.array(new_label_list)
         self.labels = np.hstack((self.labels, labels_to_add))
 
         self.size = len(self.labels)
-        self.support.update(new_label)
+        self.support.update(new_label_list)
         self.cardinality = len(self.support)
 
         new_order = np.zeros((self.size, self.size), dtype=np.int8)
@@ -483,7 +531,7 @@ class POMSet (object):
         self.order = new_order
 
     def add_dependencies_from(self, new_dependencies_list):
-        '''Add a number of new dependency relations from an iterable
+        """Add a number of new dependency relations from an iterable
         of dependencies. Each element of the iterable should be a tuple
         of 4 items:
             `(from_label, from_index, to_label, to_index)`
@@ -495,12 +543,12 @@ class POMSet (object):
         new_dependencies_list : iterable
             The new dependences to be added, each dependency specified
             as a 4-tuple of `(from_label, from_index, to_label, to_index)`.
-        '''
+        """
         for args in new_dependencies_list:
             self.add_dependency(args[0], args[2], args[1], args[3])
 
     def remove_label(self, label_to_remove, label_index=0):
-        '''Remove a label from the POMSet, updating dependency 
+        """Remove a label from the POMSet, updating dependency
         relations accordingly in the POMSet order.
 
         Parameters
@@ -514,16 +562,15 @@ class POMSet (object):
             labels, the index of the label_to_remove ;
             e.g. `element_index=3` will select the third copy of label_to_remove
             within the label list. (default 0)
-        '''
+        """
         label_to_remove_index = np.where(self.labels == label_to_remove)[0][label_index]
-        selection = range(label_to_remove_index) + \
-                    range(label_to_remove_index + 1, self.order.shape[0])
+        selection = range(label_to_remove_index) + range(label_to_remove_index + 1, self.order.shape[0])
 
         self.order = self.order[selection, :][:, selection]
         self.labels = self.labels[selection]
 
     def remove_dependency(self, from_label, to_label, from_index=0, to_index=0):
-        '''Remove a dependency from the POMSet.
+        """Remove a dependency from the POMSet.
 
         Parameters
         ----------
@@ -545,7 +592,7 @@ class POMSet (object):
             labels, the index of the to_element ;
             e.g. `to_index=3` will select the third copy of to_element
             within the label list. (default 0)
-        '''
+        """
         from_label_index = np.where(self.labels == from_label)[0][from_index]
         to_label_index = np.where(self.labels == to_label)[0][to_index]
 
@@ -554,5 +601,3 @@ class POMSet (object):
 
         if np.all(self.order == 0):
             self._is_unordered = True
-
-
